7845f1dec553f9a5dbf64436207455936b58ec59
liuchaofan <liuxiansenjs@gmail.com>
2017-08-09 01:52:22 +0800

    createStore 伪代码

--- WhyCallReducer.js
+++ WhyCallReducer.js
@@ -16,3 +16,31 @@ action.reduce((status, act)=> {
     v: 1,
     a: 100
 })
+
+
+//以下代码来自阮一峰的博客，注释为lcfme(github.com/lcfme)添加
+
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+//dispatch 函数负责调用reducer函数更新状态
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+//subscript负责想监听列表里面注册state改变后的回调事件
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    }
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};

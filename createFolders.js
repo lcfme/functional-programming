d9776077852191809af343ceb97ee9a3c349cabd
liuchaofan <liuxiansenjs@gmail.com>
2017-08-11 15:54:16 +0800

    commit readdir

--- createFolders.js
+++ createFolders.js
@@ -1,13 +1,37 @@
-const path = require('path')
 const fs = require('fs')
-module.exports.createFolder = (dir, callback) => {
+const path = require('path')
+
+const maketree = (dirname) => {
+  const _f = (self) => {
+    return (dirname) => {
+      var _n = {}
+      _n._filelist = []
+      try {
+        var fileList = fs.readdirSync(dirname)
+      } catch (e) {
+        return {}
+      }
+      fileList.forEach((file) => {
+        if (fs.statSync(path.resolve(dirname, file)).isDirectory()) {
+          _n[file] = self(self)(path.resolve(dirname, file))
+        } else {
+          _n._filelist.push(file)
+        }
+      })
+      return _n
+    }
+  }
+  return _f(_f)(dirname)
+}
+
+const createFolder = (dir, callback) => {
   const _f = (self) => {
     return (dir) => {
       return (callback) => {
         fs.stat(dir, (err, info) => {
           if (err) {
-            self(self)(path.dirname(dir))(()=>{
-              fs.mkdir(dir, ()=> {
+            self(self)(path.dirname(dir))(() => {
+              fs.mkdir(dir, () => {
                 callback && callback(info)
               })
             })
